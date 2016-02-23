f4a0908c9ca4fbf824f7d2dd191d5284dcb68d74
Mr. S-K <bsk@ms223.org>
2016-02-22 22:15:34 -0500

    bugs!

---
+++ Feb23PythonDebugging.py
@@ -0,0 +1,23 @@
+score = 0
+panda_bears = raw_input("what colors are panda bears?")
+if panda_bears.lower() == "black and white":
+  print "Nice!"
+score = score + 1 # When is the computer adding 1 to score?
+else: # Why is it saying "invalid syntax" here?
+  print "seriously? -_-"
+
+tacos = raw_input("do i like tacos? a)yes b)no")
+if tacos.lower() == a:
+  print "you know me so well"
+score = score + 1
+else:
+  print "stranger much..."
+
+school_subject = raw_input("what is my favorite school subject? a)math b)science c)gym d)global e)english")
+if school_subject.lower() == a or e:
+  print "nice job lol"
+score = score + 1
+else:
+  print "uhm, no."
+
+#remember this?
