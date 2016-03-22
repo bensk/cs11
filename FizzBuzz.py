2a466e2675180c85ce8c6be2cdcabc58d30fb83f
Mr. S-K <bsk@ms223.org>
2016-03-21 22:06:19 -0400

    fizz buzz!

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+# Print the numbers 1 → 100 except...
+# For multiples of 3, print "Fizz" instead of the number
+# For multiples of 5, print "Buzz" instead of the number
+# For multiples of 3 & 5, print "FizzBuzz" instead of the number
+# Start here:
+for groot in range(1,101):
+    print groot
+# How do you check if a number is divisible by 3?
+# (Hint: Would the REMAINDER be?)
+# Good luck! Iterate!
