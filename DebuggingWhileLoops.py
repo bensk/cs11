34258c231ca67360b73ab691ad7982f3086cb71e
Mr. S-K <bsk@ms223.org>
2016-03-31 21:21:09 -0400

    added 4/1 Do Now

---
+++ DebuggingWhileLoops.py
@@ -0,0 +1,26 @@
+# For each example below, predict what will be printed. Then, run the code and confirm your prediction.
+
+a = 0
+while a< 100:
+	print a
+"""
+Prediction:
+Observation:
+ """
+
+a = 0
+while a < 100:
+	a = a + 1
+	print a
+"""
+Prediction:
+Observation:
+ """
+
+a = raw_input("Would you like to quit: ")
+while a != "y":
+	a = raw_input("Would you like to quit: ")
+"""
+Prediction:
+Observation:
+ """
