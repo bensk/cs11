820959d138c15954271ce357fceb047a16710995
Mr. S-K <bsk@ms223.org>
2016-05-01 15:52:05 -0400

    dvwld

--- Code Examples/d_vwl.py
+++ Code Examples/d_vwl.py
@@ -1,15 +1,17 @@
 def de_vowel(a_string):
 	# your code goes here
-    strng = []
-    vowels = ['a','e','i','o','u']
-    for n in a_string:
+	strng = []
+	vowels = ['a','e','i','o','u']
+	for n in a_string:
         # if n != 'a' and n != 'e' and n != 'i' and n != 'o' and n != 'u':
-        if n not in vowels: # more elegant with a list
-            strng.append(n)
-    return ''.join(strng)
+		if n not in vowels: # more elegant with a list
+			strng.append(n)
+	return ''.join(strng)
 
-while user_input !=exit: # while loop test
-    user_input = "give me a sentence"
-    print de_vowel(user_input)
+user_input = raw_input("give me a sentence \n")
+
+while user_input != 'EXIT': # while loop test
+	print de_vowel(user_input)
+	user_input = raw_input("give me a sentence \n")
 
 # examples go here
