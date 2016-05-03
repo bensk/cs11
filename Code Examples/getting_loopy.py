266747f5ee8b87da921e26a926c04c86f1b7576a
Ben Samuels-Kalow <bensamuelskalow@gmail.com>
2016-05-03 07:21:40 -0400

    ruh roh

---
+++ Code Examples/getting_loopy.py
@@ -0,0 +1,17 @@
+"""
+Write a function `fruit_pluralizer`. It will take in a list of fruit and return nothing. The function should update the values of the list so that the values are plural. If the fruit name ends in a 'y' remove the 'y' and add 'ies', otherwise add an 's'.
+
+1. Create the function contract for `fruit_pluralizer`.
+2. Provide a few examples that confirm `fruit_pluralizer` works as expected:
+	* Include examples with 'berry'
+	* What if the list is empty?
+	* What if the fruit ends in 's'?
+"""
+
+def fruit_pluralizer(list_of_fruit):
+    for fruits in list_of_fruit:
+        print fruits.index(len(fruits))
+
+
+
+fruit_pluralizer(['berry'])
--- _site/Code Examples/TestingPush.py
+++
@@ -1,5 +0,0 @@
-# This is a test, this is only a test.
-
-# And how!
-
-#And now this!
