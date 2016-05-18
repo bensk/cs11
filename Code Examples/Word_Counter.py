57fc9b144293fd04d763753bc555d1a515ab6dca
Mr. S-K <bsk@ms223.org>
2016-05-18 06:18:42 -0400

    new lines

--- Code Examples/Word_Counter.py
+++ Code Examples/Word_Counter.py
@@ -1,7 +1,7 @@
-text_input = raw_input("Paste your text here")
+text_input = raw_input("Paste your text here \n")
 text_input=text_input.lower()
 text_input=text_input.split(" ")
-key_words = raw_input("What words do you want to search for?").split()
+key_words = raw_input("What words do you want to search for? \n").split()
 my_dictionary={}
 """
 Thought I needed this...
@@ -15,5 +15,5 @@ for n in text_input:
     if n in key_words:
         my_dictionary[n]=text_input.count(n)
 
-print "Here's the words you're curious about:"
+print "Here's the words you're curious about: \n"
 print my_dictionary
