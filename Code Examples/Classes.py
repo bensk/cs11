d44593bc9483b08432e02614b9b5928c8180049d
Ben Samuels-Kalow <bensamuelskalow@gmail.com>
2016-05-24 09:13:27 -0400

    lesson title

--- Code Examples/Classes.py
+++ Code Examples/Classes.py
@@ -1,42 +1,24 @@
-class Point(object):
-    """Represents a point in 2-D space."""
-
-print Point
-
-blank = Point()
-blank1 = Point()
-print blank
-print blank1
-
-blank.x = 42
-blank.y = 21
-
-print blank
-
-# Example
-1. Read the following code:
-
-```python
 class Pet(object):
     """Represents a pet"""
 
 my_pet_1 = Pet()
-my_pet_1.type = 'cat'
-my_pet_1.noise = 'meow'
-my_pet_1.full_name = 'Snuffles McGruff'
+my_pet_1.type = 'direwolf'
+my_pet_1.noise = 'howl'
+my_pet_1.full_name = 'Ghost'
 
 my_pet_2 = Pet()
-my_pet_2.type = 'cat'
-my_pet_2.noise = 'meow'
-my_pet_2.full_name = 'Snowpounce Flury'
+my_pet_2.type = 'direwolf'
+my_pet_2.noise = 'howl'
+my_pet_2.full_name = 'Nymeria'
 
 my_pet_3 = Pet()
-my_pet_3.type = 'cat'
-my_pet_3.noise = 'meow'
-my_pet_3.full_name = 'Snickers Snorkel'
+my_pet_3.type = 'direwolf'
+my_pet_3.noise = 'howl'
+my_pet_3.full_name = 'Summer'
 
 my_pets = [my_pet_1, my_pet_2, my_pet_3]
-print(type(my_pets))
+print type(my_pets)
+print type(my_pet_1)
+print type('Summer')
 for pet in my_pets:
-    print(pet.full_name)
-```
+    print pet.full_name
