8284bdd49e5142a3c9ffc9e5f0f31a5cbf486522
bsk <bensamuelskalow@gmail.com>
2016-06-03 08:45:23 -0400

    fixed, maybe

--- Code Examples/MethodsLab.py
+++ Code Examples/MethodsLab.py
@@ -1,13 +1,19 @@
 class Kangaroo(object):
 	def __init__(self,pouch_contents):
-		self.pouch_contents = pouch_contents
-		pouch_contents = []
-	def __put_in_pouch__(self):
-		pouch_contents.append(self)
-	def __str__(self):
-		# return str(Kangaroo)
-		return str(self.pouch_contents)
+		self.pouch_contents = []
+	def __put_in_pouch__(self,things):
+		self.pouch_contents.append(things)
 
-kanga = Kangaroo("roo")
 
-print kanga
+    # def __put_in_pouch__(self,other):
+    #     Kangaroo.pouch_contents.append(other)
+    #
+    # def __str__(self):
+    #     # return str(Kangaroo)
+    #     return str(self.pouch_contents)
+justin = Kangaroo("stuff")
+print justin.pouch_contents
+# print kanga.pouch_contents()
+
+justin.__put_in_pouch__("something")
+print justin.pouch_contents
